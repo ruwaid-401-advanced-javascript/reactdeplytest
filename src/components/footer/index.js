655d1bfef2a2de409a442304ee84baf1caa08b58
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-29 22:55:55 +0300

    test token

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Footer extends React.Component {
   render() {
-    return <footer>&copy; 2020 Code Fellows</footer>;
+    return <footer>&copy; 2021 Code Fellows</footer>;
   }
 }
 
