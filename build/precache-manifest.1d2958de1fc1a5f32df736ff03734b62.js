ed37bf66efc5e0ae55ce0d0de647520ac1db259f
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-29 21:57:17 +0300

    add yml

---
+++ build/precache-manifest.1d2958de1fc1a5f32df736ff03734b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69429156a53277e066f191eddccb388",
+    "url": "/reactdeplytest/index.html"
+  },
+  {
+    "revision": "1169eba278a60320e3bf",
+    "url": "/reactdeplytest/static/css/main.deb73a8a.chunk.css"
+  },
+  {
+    "revision": "a22af44a54a2f60e170e",
+    "url": "/reactdeplytest/static/js/2.ffbf77b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdeplytest/static/js/2.ffbf77b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1169eba278a60320e3bf",
+    "url": "/reactdeplytest/static/js/main.be8a22de.chunk.js"
+  },
+  {
+    "revision": "c25489382bff616fe50a",
+    "url": "/reactdeplytest/static/js/runtime-main.b23aa541.js"
+  }
+]);
