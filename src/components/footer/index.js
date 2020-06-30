83e28eb09a0d20794b1783eba01dd1ff25caad2e
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-30 13:18:11 +0300

    test again

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Footer extends React.Component {
   render() {
-    return <footer>&copy; 777 Code Fellows</footer>;
+    return <footer>&copy; 8888 Code Fellows</footer>;
   }
 }
 
