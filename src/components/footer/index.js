6b4eba0e19316da9ef416d5a71a5ab731737e2f0
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-30 21:16:38 +0300

    gh

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Footer extends React.Component {
   render() {
-    return <footer>&copy; 9999 Code Fellows</footer>;
+    return <footer>&copy; 1212 Code Fellows</footer>;
   }
 }
 
